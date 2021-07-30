376c2dab6a0e6d2a4b7af6e73803f8a45de5a642
bucsistvan <bucsistvan99@gmail.com>
2021-07-30 10:35:29 +0200

    A bean for the BlogPost has been added to the project

---
+++ src/main/java/hu/ulyssys/java/course/blog/demo/blogger/mbean/BlogPostCRUDMbean.java
@@ -0,0 +1,76 @@
+package hu.ulyssys.java.course.blog.demo.blogger.mbean;
+
+import hu.ulyssys.java.course.blog.demo.blogger.entity.BlogPost;
+import hu.ulyssys.java.course.blog.demo.blogger.service.BlogPostService;
+import org.primefaces.PrimeFaces;
+
+import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import java.io.Serializable;
+import java.util.List;
+
+@Named
+@ViewScoped
+public class BlogPostCRUDMbean implements Serializable {
+
+    private List<BlogPost> list;
+
+    private BlogPost selectedBlogPost;
+
+    @Inject
+    private BlogPostService blogPostService;
+
+    @PostConstruct
+    private void init(){
+        list = blogPostService.getAll();
+        selectedBlogPost = new BlogPost();
+    }
+
+    public void initSave() {
+        selectedBlogPost = new BlogPost();
+    }
+
+
+    public void save() {
+        if (selectedBlogPost.getId() == null) {
+            selectedBlogPost.setId(System.currentTimeMillis());
+            blogPostService.add(selectedBlogPost);
+            list = blogPostService.getAll();
+            selectedBlogPost = new BlogPost();
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Sikeres hozzáadás"));
+        } else {
+            blogPostService.update(selectedBlogPost);
+            list = blogPostService.getAll();
+            selectedBlogPost = new BlogPost();
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Sikeres módosítás"));
+        }
+        PrimeFaces.current().executeScript("PF('authorDialog').hide()");
+    }
+
+    public void remove(){
+        blogPostService.remove(selectedBlogPost);
+        list = blogPostService.getAll();
+        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Sikeres törlés"));
+        selectedBlogPost = new BlogPost();
+    }
+
+    public List<BlogPost> getList() {
+        return list;
+    }
+
+    public void setList(List<BlogPost> list) {
+        this.list = list;
+    }
+
+    public BlogPost getSelectedBlogPost() {
+        return selectedBlogPost;
+    }
+
+    public void setSelectedBlogPost(BlogPost selectedBlogPost) {
+        this.selectedBlogPost = selectedBlogPost;
+    }
+}
